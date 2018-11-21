e096187f1aaae0441e2121350f6e90f048e4805c
Garttox <garto25@gmail.com>
2018-11-21 15:02:05 +0100

    Revert "máma"
    This reverts commit 39ebac3a442de8582394a0091dcc15256de526dc.

--- temp/cache/Nette.RobotLoader/0ca6ead72e58635cd7813fe3486c3072.php
+++ temp/cache/Nette.RobotLoader/0ca6ead72e58635cd7813fe3486c3072.php
@@ -2,80 +2,65 @@
 return array (
   0 => 
   array (
-    'App\\Acl\\Acl' => 
-    array (
-      'file' => 'D:\\www\\android-project-server\\app\\acl\\Acl.php',
-      'time' => 1542797281,
-    ),
     'App\\Forms\\FormFactory' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\forms\\FormFactory.php',
-      'time' => 1542026085,
+      'time' => 1540371506,
     ),
     'App\\Forms\\SignInFormFactory' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\forms\\SignInFormFactory.php',
-      'time' => 1542026085,
+      'time' => 1540375319,
     ),
     'App\\Forms\\SignUpFormFactory' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\forms\\SignUpFormFactory.php',
-      'time' => 1542026085,
-    ),
-    'App\\Model\\TourManager' => 
-    array (
-      'file' => 'D:\\www\\android-project-server\\app\\model\\TourManager.php',
-      'time' => 1542026085,
-    ),
-    'App\\Model\\UserManager' => 
-    array (
-      'file' => 'D:\\www\\android-project-server\\app\\model\\UserManager.php',
-      'time' => 1542026085,
-    ),
-    'App\\Model\\DuplicateNameException' => 
-    array (
-      'file' => 'D:\\www\\android-project-server\\app\\model\\UserManager.php',
-      'time' => 1542026085,
-    ),
-    'App\\Presenters\\ApiPresenter' => 
-    array (
-      'file' => 'D:\\www\\android-project-server\\app\\presenters\\ApiPresenter.php',
-      'time' => 1542627739,
+      'time' => 1540371506,
     ),
     'App\\Presenters\\BasePresenter' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\presenters\\BasePresenter.php',
-      'time' => 1542026085,
+      'time' => 1540371506,
     ),
     'App\\Presenters\\Error4xxPresenter' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\presenters\\Error4xxPresenter.php',
-      'time' => 1542026085,
+      'time' => 1540371506,
     ),
     'App\\Presenters\\ErrorPresenter' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\presenters\\ErrorPresenter.php',
-      'time' => 1542026085,
+      'time' => 1540371506,
     ),
     'App\\Presenters\\HomepagePresenter' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\presenters\\HomepagePresenter.php',
-      'time' => 1542026085,
+      'time' => 1540371987,
     ),
     'App\\Presenters\\ListPresenter' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\presenters\\ListPresenter.php',
-      'time' => 1542627739,
+      'time' => 1540986239,
     ),
     'App\\Presenters\\SignPresenter' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\presenters\\SignPresenter.php',
-      'time' => 1542627739,
+      'time' => 1540376157,
     ),
     'App\\RouterFactory' => 
     array (
       'file' => 'D:\\www\\android-project-server\\app\\router\\RouterFactory.php',
-      'time' => 1542026085,
+      'time' => 1540371506,
+    ),
+    'App\\Model\\UserManager' => 
+    array (
+      'file' => 'D:\\www\\android-project-server\\app\\model\\UserManager.php',
+      'time' => 1540986811,
+    ),
+    'App\\Model\\DuplicateNameException' => 
+    array (
+      'file' => 'D:\\www\\android-project-server\\app\\model\\UserManager.php',
+      'time' => 1540986811,
     ),
   ),
   1 => 
