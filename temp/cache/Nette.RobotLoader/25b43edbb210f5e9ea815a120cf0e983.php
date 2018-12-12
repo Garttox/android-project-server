0ac5f0f8649c8801a5bb1e8760b39c18a44f8a4f
Donn Trumpino <dragonoidus@gmail.com>
2018-12-12 22:49:01 +0100

    a

---
+++ temp/cache/Nette.RobotLoader/25b43edbb210f5e9ea815a120cf0e983.php
@@ -0,0 +1,90 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Acl\\Acl' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\acl\\Acl.php',
+      'time' => 1544636917,
+    ),
+    'App\\Forms\\FormFactory' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\forms\\FormFactory.php',
+      'time' => 1544636917,
+    ),
+    'App\\Forms\\SignInFormFactory' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\forms\\SignInFormFactory.php',
+      'time' => 1544636917,
+    ),
+    'App\\Forms\\SignUpFormFactory' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\forms\\SignUpFormFactory.php',
+      'time' => 1544636917,
+    ),
+    'App\\Model\\TourManager' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\model\\TourManager.php',
+      'time' => 1544636917,
+    ),
+    'App\\Model\\UserManager' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\model\\UserManager.php',
+      'time' => 1544636917,
+    ),
+    'App\\Model\\DuplicateNameException' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\model\\UserManager.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\AdminPresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\AdminPresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\ApiPresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\ApiPresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\BasePresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\BasePresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\Error4xxPresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\Error4xxPresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\ErrorPresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\ErrorPresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\HomepagePresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\HomepagePresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\ListPresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\ListPresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\Presenters\\SignPresenter' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\presenters\\SignPresenter.php',
+      'time' => 1544636917,
+    ),
+    'App\\RouterFactory' => 
+    array (
+      'file' => 'E:\\SchoolProject\\android-project-server\\app\\router\\RouterFactory.php',
+      'time' => 1544636917,
+    ),
+  ),
+  1 => 
+  array (
+    'Nette\\Environment' => 3,
+  ),
+);
---
+++ temp/cache/latte/templates-List-default.latte--68cfc79993.php
@@ -0,0 +1,185 @@
+<?php
+// source: E:\SchoolProject\android-project-server\app\presenters/templates/List/default.latte
+
+use Latte\Runtime as LR;
+
+class Template68cfc79993 extends Latte\Runtime\Template
+{
+	public $blocks = [
+		'content' => 'blockContent',
+		'_toursListContainer' => 'blockToursListContainer',
+	];
+
+	public $blockTypes = [
+		'content' => 'html',
+		'_toursListContainer' => 'html',
+	];
+
+
+	function main()
+	{
+		extract($this->params);
+		if ($this->getParentName()) return get_defined_vars();
+		$this->renderBlock('content', get_defined_vars());
+		return get_defined_vars();
+	}
+
+
+	function prepare()
+	{
+		extract($this->params);
+		if (isset($this->params['id'])) trigger_error('Variable $id overwritten in foreach on line 6');
+		if (isset($this->params['row'])) trigger_error('Variable $row overwritten in foreach on line 6');
+		Nette\Bridges\ApplicationLatte\UIRuntime::initialize($this, $this->parentName, $this->blocks);
+		
+	}
+
+
+	function blockContent($_args)
+	{
+		extract($_args);
+?>
+<div class="container text-center">
+    <h1><hr>Seznam Stezek<hr></h1>
+    <div class="accordion" id="accordion">
+        <div<?php echo ' id="' . htmlSpecialChars($this->global->snippetDriver->getHtmlId('toursListContainer')) . '"' ?>>
+<?php $this->renderBlock('_toursListContainer', $this->params) ?>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content text-center">
+      <div class="modal-header">
+        <h5 class="modal-title text-center" id="exampleModalLabel">QR</h5>
+        <a class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </a>
+      </div>
+      <div class="modal-body">
+        
+      </div>
+      <div class="modal-footer">
+        <a class="btn btn-secondary" data-dismiss="modal">Zavřít</a>
+        <a class="btn btn-primary text-white">Vytisknout</a>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+	}
+
+
+	function blockToursListContainer($_args)
+	{
+		extract($_args);
+		$this->global->snippetDriver->enter("toursListContainer", "static");
+		?>            <?php
+		$iterations = 0;
+		foreach ($data as $id => $row) {
+?>
+
+            <div class="card"<?php echo ' id="' . htmlSpecialChars($this->global->snippetDriver->getHtmlId("row-$id")) . '"' ?>>
+<?php
+			$this->global->snippetDriver->enter("row-$id", "dynamic");
+			?>                <?php
+			if ($row['author'] == $user->getIdentity()->username) {
+?>
+
+                <div class="card-header bg-own" id="heading<?php echo LR\Filters::escapeHtmlAttr($row['id']) /* line 9 */ ?>">
+<?php
+			}
+			else {
+				?>                <div class="card-header" id="heading<?php echo LR\Filters::escapeHtmlAttr($row['id']) /* line 11 */ ?>">
+<?php
+			}
+?>
+                    <p class="card-title text-left">
+                    <strong class="leftText"><?php echo LR\Filters::escapeHtmlText($row['title']) /* line 14 */ ?></strong> <strong>|</strong> <em class="card-subtitle mb-2 text-muted"><?php
+			echo LR\Filters::escapeHtmlText($row['author']) /* line 14 */ ?></em> <strong>|</strong> 
+                    <small class="text-muted">
+<?php
+			$this->global->switch[] = ($row['published']);
+			if (FALSE) {
+			}
+			elseif (end($this->global->switch) === ('yes')) {
+?>
+                                Zveřejněno
+<?php
+			}
+			elseif (end($this->global->switch) === ('no')) {
+?>
+                                Nezveřejněno
+<?php
+			}
+			elseif (end($this->global->switch) === ('wip')) {
+?>
+                                Rozpracované
+<?php
+			}
+			array_pop($this->global->switch) ?>
+                    </small>  
+                        <span class="btn-group right" role="group" aria-label="Basic example">
+<?php
+			if ($user->isInRole('admin')) {
+				?>                                <a class="btn btn-dark list-button text-white" data-toggle="modal" data-target="#exampleModal" data-whatever="<?php
+				echo LR\Filters::escapeHtmlAttr($row['qr']) /* line 27 */ ?>" data-name="<?php echo LR\Filters::escapeHtmlAttr($row['title']) /* line 27 */ ?>"><i class="fas fa-qrcode"></i> QR</a>
+<?php
+			}
+			if ($user->isInRole('admin') || ($user->isInRole('editor') && $row['author'] == $user->getIdentity()->username)) {
+				?>                                <a class="btn btn-warning list-button" href="<?php echo LR\Filters::escapeHtmlAttr($this->global->uiControl->link("List:detail", [$row['id']])) ?>"><i class="fas fa-pen"></i> Upravit</a>
+<?php
+			}
+			?>                            <a class="btn btn-primary list-button" data-toggle="collapse" data-target="#collapse<?php
+			echo LR\Filters::escapeHtmlAttr($row['id']) /* line 32 */ ?>" aria-expanded="true" aria-controls="collapse<?php
+			echo LR\Filters::escapeHtmlAttr($row['id']) /* line 32 */ ?>"><i class="fas fa-chevron-down"></i> Více</a>  
+                        </span>    
+<?php
+			if (($row['published'] == 'no' && $user->isInRole('admin'))) {
+				?>                            <a class="btn btn-success right ajax list-button" href="<?php echo LR\Filters::escapeHtmlAttr($this->global->uiControl->link("publish!", [$id])) ?>"><i class="fas fa-check"></i> Zveřejnit</a>
+<?php
+			}
+			elseif (($row['published'] == 'yes' && $user->isInRole('admin'))) {
+				?>                            <a class="btn btn-danger right ajax list-button" href="<?php echo LR\Filters::escapeHtmlAttr($this->global->uiControl->link("publish!", [$id])) ?>"><i class="fas fa-times"></i> Stáhnout</a>
+<?php
+			}
+			elseif (($row['published'] == 'wip' && $row['author'] == $user->getIdentity()->username)) {
+				?>                            <a class="btn btn-secondary right ajax list-button" href="<?php echo LR\Filters::escapeHtmlAttr($this->global->uiControl->link("publish!", [$id])) ?>"><i class="fas fa-flag-checkered"></i> Dokončit</a> 
+<?php
+			}
+			if ($row['author'] != $user->getIdentity()->username && !$user->isInRole('admin')) {
+?>
+                            <i class="rightText">Nejste autorem této stezky, nemůžete provádět úpravy.</i>
+<?php
+			}
+?>
+                    </p> 
+<?php
+			if ($row['author'] == $user->getIdentity()->username) {
+?>
+                </div>
+<?php
+			}
+			else {
+?>
+                </div>
+<?php
+			}
+			?>                 <div id="collapse<?php echo LR\Filters::escapeHtmlAttr($row['id']) /* line 50 */ ?>" class="collapse card-body" aria-labelledby="heading<?php
+			echo LR\Filters::escapeHtmlAttr($row['id']) /* line 50 */ ?>" data-parent="#accordion">
+                    <p>Duis duis duis tempor et elit eiusmod irure do eu minim sint. Id aliqua nostrud et eu excepteur ad dolore commodo incididunt aliqua commodo id non. Exercitation voluptate laborum elit commodo mollit pariatur. Ad aliquip excepteur voluptate laboris excepteur. Aliquip in mollit ipsum excepteur. Qui ad ad veniam mollit consequat incididunt non id laborum fugiat consectetur officia proident.</p>
+                </div> 
+                
+<?php
+			$this->global->snippetDriver->leave();
+?>            </div> 
+<?php
+			$iterations++;
+		}
+		$this->global->snippetDriver->leave();
+		
+	}
+
+}
